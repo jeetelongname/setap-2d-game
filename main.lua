c028def0df09035c541a964eba4c2ba13b6c833c
imra1998 <114669255+imra1998@users.noreply.github.com>
2023-02-21 12:43:48 +0000

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,6 +1,16 @@
 Serialize = require("lib/ser")
 function Savefile()
     local save = {} -- created a table to store variables
+    local progress 
+    local worldmap 
+    local currentinventory
+    local chests
+    local usersposition
+    local spawnedNPC
+    local items
+    local currentpoints
+    local stats
+    save.Savefile = Savefile
     save.progress = progress
     save.worldmap = worldmap
     save.currentinventory = currentinventory
@@ -12,5 +22,8 @@ function Savefile()
     save.stats = stats
     Savefile.filesystem.write ("savegame.txt", Serialize(save))-- saves the table in the savegame
 end
+function Savefile()
+local save = Savefile.filesystem.load("savegame.txt") ()-- loads the data table
+Savefile = save.Savefile       
+end 
 
-function
