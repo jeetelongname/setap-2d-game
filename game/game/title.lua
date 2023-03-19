d1b661ce8288f774d94df0d1151e52fa9de1a36d
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2023-03-19 00:59:30 +0000

    feat(title): basic title screen
    Incorporate a simple title and a couple of buttons, this can now be
    easily worked on as functionality is added

--- game/game/title.lua
+++ game/game/title.lua
@@ -2,24 +2,43 @@ M = {}
 
 Types = require("lib.types")
 Button = require("lib.button")
-local message = "hello"
+Message = "hello"
+
+-- when clicked, return true to start the game
+local startButton = Button:new { text = { { 0, 0, 0 }, "Start Game" }, x = 300, y = 250 }
+
+-- this button toggles a message, its means to be a bit more of a full represenation of how to use the buttons
+local messageButton = Button:new { text = { { 0, 0, 0 }, "toggle greeting" }, x = 300, y = 310,
+  callback = function()
+    if Message == "hello" then
+      Message = "hi"
+    else
+      Message = "hello"
+    end
+  end }
+
 
 M.update = function()
-
-  startButton = Button:new()
-
   local pred = startButton:check()
+  messageButton:check()
 
   if pred then
+    -- next module
     return Types.modules.quit
   else
+    -- otherwise keep showing the title
     return Types.modules.title
   end
 end
 
 M.draw = function()
-  love.graphics.print("hello", 100, 100)
+  -- game title
+  love.graphics.print("Survival Game", 300, 200, 0, 2, 2)
+  -- greeting message
+  love.graphics.print(Message, 100, 100)
+  -- draw buttons
   startButton:draw()
+  messageButton:draw()
 end
 
 return M
