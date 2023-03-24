45c44f2c43153e47cf1d0a5316a19fb13e7c4afc
Brad Edmondson <116604028+BradEdmondson02@users.noreply.github.com>
2023-03-24 15:50:35 +0000

    feat: add gamestate
    Add gamestate class that includes map and player

---
+++ game/gamestate.lua
@@ -0,0 +1,33 @@
+require "player"
+require "map"
+
+Gamestate = {}
+
+function Gamestate:new(map, player)
+
+    local map = {}
+    local player = {}
+    
+    local obj = {
+
+        get_map = function()
+            return map
+        end,
+
+        set_map = function(setMap)
+            map = setMap
+        end,
+
+        get_player = function()
+            return player
+        end,
+
+        set_player = function(setPlayer)
+            player = setPlayer
+        end
+
+}
+setmetatable(obj, self)
+self.__index = self
+
+end
