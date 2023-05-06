119cec14e9b34b830c43c0df43a0eb3f5ae5f420
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2023-05-06 01:09:40 +0100

    feat: add in a state argument to update functions
    This argument (a table) contains any state that may need to be shared
    between modules.

--- game/main.lua
+++ game/main.lua
@@ -1,24 +1,30 @@
 local steps = {
     -- this works as require essentually resolves
     -- to the modules return value
-    require "game.title",
-    require "game.map"
+    require("game.title"),
+    require("game.map"),
+    require("game.game")
 }
-
-Types = require("lib.types")
+local json = require("deps.dkjson")
+local Types = require("lib.types")
 
 local current = Types.modules.title
+local nextModule = Types.modules.title
+
+local state = {}
+local nextState = {}
 
 function love.update()
     if love.keyboard.isDown('q') then
         love.event.push('quit')
     end
 
-    local nextModule = steps[current].update()
+    nextModule, nextState = steps[current].update(state)
     if nextModule == Types.modules.quit then
         love.event.push("quit")
     else
         current = nextModule
+        state = nextState
     end
 end
 
