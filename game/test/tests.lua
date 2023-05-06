5900b5edb83898baccb6d0033aad47a2e5388854
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2023-05-06 01:11:34 +0100

    test(map): test size

--- game/test/tests.lua
+++ game/test/tests.lua
@@ -10,4 +10,22 @@ function MapTest:testLength()
   lu.assertEquals(#self.map.tiles, 100)
 end
 
+function MapTest:toString()
+  local dummyObj = {
+    tiles = {},
+    size  = 100
+  }
+
+  for i = 1, 100 do
+    dummyObj.tiles[i] = {}
+    for j = 1, 100 do
+      dummyObj.tiles[i][j] = Tiles:new()
+    end
+  end
+
+
+
+  lu.assertEquals(self.map:toString(),)
+end
+
 os.exit(lu.LuaUnit.run())
