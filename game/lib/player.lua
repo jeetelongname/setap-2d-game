437c213556b3d5940a7b201ca2874b52238f01d4
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2023-05-06 01:17:10 +0100

    fix(player): return player class
    also
    - format file
    - make player local to the fille
    - correct spelling mistakes
    - change default position to 50, 50

--- game/lib/player.lua
+++ game/lib/player.lua
@@ -1,72 +1,57 @@
 require "lib.inventory"
 
-Player = {}
+local Player = {}
 
 function Player:new(name, movementSpeed, sprite)
-
     local name = name
-    local position = {x = 100, y = 100}
+    local position = { x = 50, y = 50 }
     local health = 100
     local movementSpeed = movementSpeed
     local sprite = sprite
 
     local obj = {
-
         inventory = Inventory:new(),
-
         get_name = function()
             return name
         end,
-
         get_sprite = function()
             return sprite
         end,
-
         get_health = function()
             return health
         end,
-
         change_health = function(changeHealthBy)
             health = health + changeHealthBy
         end,
-
-        set_health = function(seHealthTo)
+        set_health = function(setHealthTo)
             health = setHealthTo
         end,
-
         get_position = function()
             return position
         end,
-
         change_position_by_amount = function(x, y)
             position.x = position.x + x
             position.y = position.y + y
         end,
-
         set_position = function(x, y)
             position.x = x
             position.y = y
         end,
-
         get_movementSpeed = function()
             return movementSpeed
         end,
-
         change_movementSpeed = function(value)
             movementSpeed = movementSpeed + value
         end,
-
         set_movementSpeed = function(value)
             movementSpeed = value
         end
-
     }
     self.__index = self
     return setmetatable(obj, self)
-
 end
 
-
+return Player
 -- Player1 = Player:new('Jude')
 -- print(Player1.get_name())
 -- print(Player1.get_health())
