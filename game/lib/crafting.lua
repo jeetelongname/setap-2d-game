1a6758ae361ed7a8bfccbff8e274c3d377af535a
Jude <111361323+JudeWhiting@users.noreply.github.com>
2023-05-10 15:34:33 +0100

    Update crafting.lua (comments)

--- game/lib/crafting.lua
+++ game/lib/crafting.lua
@@ -1,8 +1,9 @@
-M = {} -- this file returns functions to sue for crafting, no classes.
-
-function listContainsAll(items, recipe) -- checks if all items in the recipe have been put into the function
-
-  for i=2, #recipe do -- starts at the second position in the list, as the first position is the name of the item trying to be crafted
+-- this file returns functions to use for crafting, no classes.
+M = {}
+-- checks if all items in the recipe have been put into the function
+function listContainsAll(items, recipe) 
+  -- starts at the second position in the list, as the first position is the name of the item trying to be crafted
+  for i=2, #recipe do 
 
     local found = false
     
@@ -25,7 +26,6 @@ end
 
 -- below are all of the crafting recipes for each item in the game
 -- each value in the list represents what is required to craft an item
-
 function swordRecipe()
   return {"metal", "metal", "stick"}
 end
