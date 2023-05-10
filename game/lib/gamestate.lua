d133d73eb298d5b5934b94e5d684574865a71a5b
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2023-05-10 19:28:06 +0100

    docs(gamestate): add

--- game/lib/gamestate.lua
+++ game/lib/gamestate.lua
@@ -1,22 +1,33 @@
 local Gamestate = {}
 
+-- The Gamestate holds the map and player in one object. It takes
+-- firstmap: Map
+-- firstplayer: Player
 function Gamestate:new(firstmap, firstplayer)
     local map = firstmap or {}
     local player = firstplayer or {}
 
     local obj = {
+        -- returns Map
         get_map = function()
             return map
         end,
+        -- takes Map
+        -- returns nil
         set_map = function(setMap)
             map = setMap
         end,
+        -- returns Player
         get_player = function()
             return player
         end,
+        -- takes Player
+        -- returns nil
         set_player = function(setPlayer)
             player = setPlayer
         end,
+        -- takes nothing
+        -- returns { Player, Map }
         dump = function()
             return { player = player, map = map }
         end
