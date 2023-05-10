303a807375ed87d6c23059df5ddcbce45ded3129
Jude <111361323+JudeWhiting@users.noreply.github.com>
2023-05-10 15:23:03 +0100

    Update inventory.lua (comments)

--- game/lib/inventory.lua
+++ game/lib/inventory.lua
@@ -1,30 +1,30 @@
 require "lib.item"
 
 Inventory = {}
-
+-- the inventory class stores information on what is currently in the player's inventory, and functions to manipulate itself
 function Inventory:new()
 
     local inventory = {}
-
-    for i = 1, 20 do -- creates a table (the players' inventory) with 20 slots with the value -1, to mark that the slot is empty
+    -- creates a table (the players' inventory) with 20 slots with the value -1, to mark that the slot is empty
+    for i = 1, 20 do
         inventory[i] = -1
     end
 
     local obj = {
-
-        get_inventory = function(inventoryPosition) -- returns the item at a certain position in the inventory, and how many of that item are in that slot
+        -- returns the item at a certain position in the inventory, and how many of that item are in that slot
+        get_inventory = function(inventoryPosition) 
             if inventory[inventoryPosition] ~= -1 then
                 return inventory[inventoryPosition][1], inventory[inventoryPosition][2]
             else
                 return inventory[inventoryPosition]
             end
         end,
-
+        -- returns a list of all items currently in the player's inventory, and all empty spaces
         get_inventory_all = function()
             return inventory
         end,
-
-        set_inventory = function(item) --checks for empty spaces in the inventory, and puts the item in the first space it finds
+        --checks for empty spaces in the inventory, and puts the item in the first space it finds
+        set_inventory = function(item) 
 
             for i = 1, #inventory do
 
@@ -40,8 +40,8 @@ function Inventory:new()
             return 'no space in inventory'
 
         end,
-
-        remove_inventory = function(item) -- removes a certain item from the inventory
+        -- removes a certain item from the inventory
+        remove_inventory = function(item)
 
             for i = 1, #inventory do
                 if inventory[i] == -1 then
