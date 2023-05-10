935c402e2160af2da181287743082a55a3b1d49a
Jude <111361323+JudeWhiting@users.noreply.github.com>
2023-05-10 16:06:20 +0100

    Update tests.lua (tests for player class)

--- game/test/tests.lua
+++ game/test/tests.lua
@@ -1,5 +1,6 @@
 local lu = require("deps.luaunit")
 local Maptools = require("lib.map")
+local Playertools = require("lib.player")
 
 MapTest = {}
 function MapTest:setup()
@@ -28,6 +29,45 @@ function Gametest:testBound()
   lu.assertEquals(outOfBound { x = 101, y = 101 }, true)
   lu.assertEquals(outOfBound { x = 0, y = 0 }, false)
 end
+
+Playertest = {}
+function Playertest:setup()
+  self.player = Playertools.Player:new("test", 5, {0, 244, 65})
+end
+
+function Playertest:testName()
+  lu.assertEquals(self.player.get_name(), "test")
+end
+
+function Playertest:testMovementSpeed()
+  lu.assertEquals(self.player.get_movementSpeed(), 5)
+  self.player.set_movementSpeed(2)
+  lu.assertEquals(self.player.get_movementSpeed(), 2)
+  self.player.change_movementSpeed(-1)
+  lu.assertEquals(self.player.get_movementSpeed(), 1)
+end
+
+function Playertest:testSprite()
+  lu.assertEquals(self.player.get_sprite(), {0, 244, 65})
+end
+
+function Playertest:testHealth()
+  lu.assertEquals(self.player.get_health(), 100)
+  self.player.set_health(50)
+  lu.assertEquals(self.player.get_health(), 50)
+  self.player.change_health(-20)
+  lu.asserEquals(self.player.get_health(), 10)
+end
+
+function Playertest:testPosition()
+  lu.assertEquals({self.player.get_position.x, self.player.get_position().y}, {50, 50})
+  self.player.set_position(25, 34)
+  lu.assertEquals({self.player.get_position.x, self.player.get_position().y}, {25, 34})
+  self.player.change_position_by_amount(-25,-34)
+  lu.assertEquals({self.player.get_position.x, self.player.get_position().y}, {0, 0})
+end
+
+function 
 
 Savetest = {}
 function Savetest:testsave()
